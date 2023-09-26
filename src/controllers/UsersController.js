9588d8ccb397e939768d9541853186e6193aabcc
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-09-26 14:52:48 -0300

    validated password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -43,8 +43,8 @@ class UsersController {
             throw new AppError("Este e-mail já está em uso.")
         }
 
-        user.name = name
-        user.email = email
+        user.name = name ?? user.name
+        user.email = email ?? user.email
 
         if (password && !old_password) {
             throw new AppError("Você precisa informar a senha antiga.")
@@ -65,9 +65,9 @@ class UsersController {
             name = ?,
             email = ?,
             password = ?,
-            updated_at = ?
+            updated_at = DATETIME('now')
             WHERE id = ?`,
-            [user.name, user.email, user.password, new Date(), id]
+            [user.name, user.email, user.password, id]
         )
 
         return response.json()
