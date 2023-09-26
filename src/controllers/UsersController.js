160ecab8338001f7ba1ec0262cad86c7d4228185
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-09-25 21:30:34 -0300

    Controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
     create(request, response) {
         const { name, email, passworld } = request.body
+
+        if (!name) {
+            throw new AppError("O nome é obrigatório", 200)
+        }
 
         response.status(201).json({ name, email, passworld })
     }
