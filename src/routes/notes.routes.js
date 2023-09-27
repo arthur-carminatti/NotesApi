45bbf91339cc9ae9bbff1bba75821995003edd0e
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-09-27 14:31:31 -0300

    Delete notes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -8,5 +8,6 @@ const notesControler = new NotesControler()
 
 notesRoutes.post("/:user_id", notesControler.create)
 notesRoutes.get("/:id", notesControler.show)
+notesRoutes.delete("/:id", notesControler.delete)
 
 module.exports = notesRoutes;
